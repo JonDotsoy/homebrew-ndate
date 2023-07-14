fdc5bba7884ba12507279bcc0f2228447fdab2fd
Jonathan Delgado <JonDotsoy@users.noreply.github.com>
2023-07-14 17:17:52 -0400

    Update ndate.rb

--- ndate.rb
+++ ndate.rb
@@ -1,8 +1,8 @@
 class Ndate < Formula
   desc "Date format to console"
-  homepage "https://github.com/JonDotsoy/ndate/tree/v0.2.8#readme"
-  url "https://github.com/JonDotsoy/ndate/archive/refs/tags/v0.2.8.tar.gz"
-  sha256 "2cbced6f023a4a581b012a6710215f81cb64dc7bf3a9bc7cfbd4cd8145a50f70"
+  homepage "https://github.com/JonDotsoy/ndate/tree/v0.2.9#readme"
+  url "https://github.com/JonDotsoy/ndate/archive/refs/tags/v0.2.9.tar.gz"
+  sha256 "2517d79ff4cb6113c232e080b829d3eaa967294fafc2b68756bc52759be494bd"
   license "MIT"
 
   depends_on "deno"
