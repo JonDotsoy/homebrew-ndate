74efeaa62f4fa4eaf59e7de75c03303b38f2c83e
Jonathan Delgado <JonDotsoy@users.noreply.github.com>
2023-10-28 02:08:02 -0300

    Update ndate@0.4.rb

--- ndate@0.4.rb
+++ ndate@0.4.rb
@@ -2,7 +2,7 @@ class NdateAT04 < Formula
   desc "Date format to console"
   homepage "https://github.com/JonDotsoy/ndate/tree/morgan-v0.4.0-morgan#readme"
   url "https://github.com/JonDotsoy/ndate/archive/refs/tags/morgan-v0.4.0-morgan.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "1576b578b70f0d20e0aa7cfef107f7bbeba30f5200f318bdfa8963a41f55c402"
   license "MIT"
 
   depends_on "deno"
