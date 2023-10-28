674a7178dad1acc1d21aa5bfd6ae8558648b915f
Jonathan Delgado <JonDotsoy@users.noreply.github.com>
2023-10-28 02:07:17 -0300

    Update ndate@0.4.rb

--- ndate@0.4.rb
+++ ndate@0.4.rb
@@ -1,4 +1,4 @@
-class Ndate < Formula
+class NdateAT04 < Formula
   desc "Date format to console"
   homepage "https://github.com/JonDotsoy/ndate/tree/morgan-v0.4.0-morgan#readme"
   url "https://github.com/JonDotsoy/ndate/archive/refs/tags/morgan-v0.4.0-morgan.tar.gz"
